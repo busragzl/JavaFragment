15ae7feaf1e7fc303f03703d87dcc7684133c555
busragazel <busragzl96@gmail.com>
2025-09-18 22:27:31 +0300

    fragment örneği basit şekilde

--- app/src/main/java/com/busragazel/javafragment/SecondFragment.java
+++ app/src/main/java/com/busragazel/javafragment/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +12,16 @@ import androidx.fragment.app.Fragment;
 
 public class SecondFragment extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_second,container,false);
+
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_second,container,false);
+
+        TextView textView = viewGroup.findViewById(R.id.textView);
+        textView.setText("Second Fragment Example");
+
+       return viewGroup;
     }
 }
